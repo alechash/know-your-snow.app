ee566b54025356c5e64b6e64953bb52ec7be95ef
Jude Wilson <jude@alecw.net>
2021-12-15 17:04:28 -0500

    Initial Commit

---
+++ Know Your Snow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Know Your Snow
+//
+//  Created by Jude Wilson on 12/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
